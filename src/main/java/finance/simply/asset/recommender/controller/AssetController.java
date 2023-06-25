f1a9a9100b0644a9211a091451058e0c5dda889c
Jacek Musiał <7474972+jacekmusial@users.noreply.github.com>
2023-06-25 19:17:18 +0100

    Implement endpoints

--- src/main/java/finance/simply/asset/recommender/controller/AssetController.java
+++ src/main/java/finance/simply/asset/recommender/controller/AssetController.java
@@ -1,30 +1,74 @@
 package finance.simply.asset.recommender.controller;
 
 import finance.simply.asset.recommender.model.Asset;
+import finance.simply.asset.recommender.repository.AssetRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 
 @RestController
 @RequestMapping("/api/asset")
+/**
+ * Asset controller.
+ */
 public class AssetController {
 
-  @GetMapping
-  public ResponseEntity<List<Asset>> getAssetsWithCostUnder(double maxCost) {
-    // TODO: Implement
+  /**
+   * DAO repository {@link AssetRepository}
+   */
+  private final AssetRepository assetRepository;
 
-    List<Asset> assets = new ArrayList<>();
+  /**
+   * Constructor for AssetController. Initializes the assetRepository.
+   *
+   * @param assetRepository repository for the Asset entity
+   */
+  public AssetController(AssetRepository assetRepository) {
+    this.assetRepository = assetRepository;
+  }
+
+  /**
+   * Return all assets with a cost under a specified value.
+   *
+   * @param maxCost the maximum cost
+   * @return ResponseEntity with a list of {@link Asset}s
+   */
+  @GetMapping
+  public ResponseEntity<List<Asset>> getAssetsWithCostUnder(@RequestParam double maxCost) {
+    List<Asset> assets = assetRepository.findByCostLessThanEqualOrderByCostAsc(maxCost);
+
+    if (assets.isEmpty()) {
+      return new ResponseEntity<>(Collections.emptyList(), HttpStatus.NOT_FOUND);
+    }
+
     return ResponseEntity.ok(assets);
   }
 
+  /**
+   * Return the sum of costs for each asset type.
+   *
+   * @return ResponseEntity with a map of {@link Asset.Type} to sums of costs
+   */
   @GetMapping("/sum-for-types")
   public ResponseEntity<Map<Asset.Type, Double>> getSumOfCostsForAssetTypes() {
-    // TODO: Implement
+    Map<Asset.Type, Double> assetTypeCostsMap = new EnumMap<>(Asset.Type.class);
 
-    Map<Asset.Type, Double> assetTypeCostsMap = new HashMap<>();
+    for (Asset.Type type : Asset.Type.values()) {
+      assetTypeCostsMap.put(type, 0.0);
+    }
+
+    final List<Object[]> results = assetRepository.findSumOfCostsForAssetTypes();
+    for (Object[] result : results) {
+      Asset.Type type = (Asset.Type) result[0];
+      final Double sum = (Double) result[1];
+      assetTypeCostsMap.put(type, sum);
+    }
+
     return ResponseEntity.ok(assetTypeCostsMap);
   }
 
