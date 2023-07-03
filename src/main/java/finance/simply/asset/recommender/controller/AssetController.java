302407a2dd2087f85a219925119ead2cbf574445
Sylvester Loreto <sylvester.abreu@googlemail.com>
2023-07-03 22:54:57 +0100

    Implementation of getting all assets, where the cost is less or equal, to the max cost passed as query parameter.

--- src/main/java/finance/simply/asset/recommender/controller/AssetController.java
+++ src/main/java/finance/simply/asset/recommender/controller/AssetController.java
@@ -1,31 +1,47 @@
 package finance.simply.asset.recommender.controller;
 
 import finance.simply.asset.recommender.model.Asset;
+import finance.simply.asset.recommender.repository.AssetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/asset")
 public class AssetController {
 
-  @GetMapping
-  public ResponseEntity<List<Asset>> getAssetsWithCostUnder(double maxCost) {
-    // TODO: Implement
+    private final AssetRepository assetRepository;
 
-    List<Asset> assets = new ArrayList<>();
-    return ResponseEntity.ok(assets);
-  }
+    @Autowired
+    public AssetController(AssetRepository assetRepository) {
+        this.assetRepository = assetRepository;
+    }
 
-  @GetMapping("/sum-for-types")
-  public ResponseEntity<Map<Asset.Type, Double>> getSumOfCostsForAssetTypes() {
-    // TODO: Implement
+    @GetMapping
+    public ResponseEntity<List<Asset>> getAssetsWithCostUnder(
+            @RequestParam(required = true) double maxCost) {
+        final List<Asset> assets = assetRepository.findAll().stream()
+                .filter(asset -> asset.getCost() <= maxCost)
+                .sorted(Comparator.comparing(Asset::getName))
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(assets);
+    }
 
-    Map<Asset.Type, Double> assetTypeCostsMap = new HashMap<>();
-    return ResponseEntity.ok(assetTypeCostsMap);
-  }
+    @GetMapping("/sum-for-types")
+    public ResponseEntity<Map<Asset.Type, Double>> getSumOfCostsForAssetTypes() {
+        // TODO: Implement
+
+        Map<Asset.Type, Double> assetTypeCostsMap = new HashMap<>();
+        return ResponseEntity.ok(assetTypeCostsMap);
+    }
 
 }
